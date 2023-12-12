e55a3d57696fe98fd887a1bab0a79e40305e946d
Marchell Halim <marchellhalim@gmail.com>
2023-12-12 13:49:53 +0700

    push all project files

---
+++ app/src/main/java/com/example/pregnagrowth/PasswordEditText.kt
@@ -0,0 +1,31 @@
+package com.example.pregnagrowth
+
+import android.content.Context
+import android.util.AttributeSet
+import androidx.appcompat.widget.AppCompatEditText
+import androidx.core.widget.addTextChangedListener
+
+class PasswordEditText : AppCompatEditText {
+    constructor(context: Context) : super(context) {
+        init()
+    }
+
+    constructor(context: Context, attrs: AttributeSet) : super(context, attrs) {
+        init()
+    }
+
+    constructor(context: Context, attrs: AttributeSet, defStyleAttr: Int) : super(context, attrs, defStyleAttr) {
+        init()
+    }
+
+    private fun init() {
+        addTextChangedListener(onTextChanged = { s, _, _, _ ->
+            if(s.toString().length < 8) {
+                setError("Password tidak boleh kurang dari 8 karakter", null)
+            }
+            else {
+                error = null
+            }
+        })
+    }
+}
